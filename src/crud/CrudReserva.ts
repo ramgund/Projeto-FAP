c7bc62467d42ebb5d1e84f1c406d85676a52459c
Stefany Gomes <145521642+stefanyGomes-dev@users.noreply.github.com>
2024-08-18 18:29:25 -0300

    Create CrudReserva.ts
    Adicionando classes Crud de Reserva.

---
+++ src/crud/CrudReserva.ts
@@ -0,0 +1,25 @@
+import { Reserva } from "../models/Reserva";
+
+class CrudReserva {
+    private listaDeReservas: Array<Reserva>;
+
+    constructor() {
+        this.listaDeReservas = new Array<Reserva>();
+    }
+
+    public adicionarReserva() {
+
+    }
+
+    public removerReserva() {
+
+    }
+
+    public buscarReserva() {
+
+    }
+
+    public atualizarReserva() {
+        
+    }
+}
