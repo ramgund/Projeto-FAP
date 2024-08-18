262ee94788483d52984a5837571fcc8bf372672f
Stefany Gomes <145521642+stefanyGomes-dev@users.noreply.github.com>
2024-08-18 18:29:14 -0300

    Create CrudHospede.ts
    Adicionando classes Crud de Hospede.

---
+++ src/crud/CrudHospede.ts
@@ -0,0 +1,25 @@
+import { Hospede } from "../models/Hospede";
+
+class CrudHospedes {
+    private listaDeHospedes: Array<Hospede>;
+
+    constructor() {
+        this.listaDeHospedes = new Array<Hospede>();
+    }
+
+    public adicionarHospede() {
+
+    }
+
+    public removerHospede() {
+
+    }
+
+    public buscarHospede() {
+
+    }
+
+    public atualizarHospede() {
+        
+    }
+}
