6a1e83da1dddc3ff68eba3369d67418a6035bd94
ramgund <vtwolmer9@gmail.com>
2024-08-19 22:38:16 -0300

    Add cadastro quarto

--- app.ts
+++ app.ts
@@ -3,75 +3,74 @@ import { Hospedes } from "./src/models/Hospedes";
 import { Quarto } from "./src/models/Quarto";
 import { Reserva } from "./src/models/Reserva";
 import {
-  cadastroResposta,
+  cadastroOpcoes,
   menu,
-  opcaoDeletar,
-  opcaoListar,
-  respostaMenu,
-  opcaoAtualizar,
+  deletarOpcoes,
+  listarOpcoes,
+  atualizarOpcoes,
 } from "./src/Functions/functions";
 
 const hospede: Hospedes = new Hospedes();
 const reserva: Reserva = new Reserva();
 const quarto: Quarto = new Quarto();
 
+let respostaMenu: number = menu();
+
 while (respostaMenu !== 5) {
-  menu();
   switch (respostaMenu) {
     case 1:
-      if (cadastroResposta == 1) {
+      const cadastroResposta = cadastroOpcoes();
+      if (cadastroResposta === 1) {
         reserva.cadastrarReserva(hospede, quarto);
         console.log("Reserva cadastrada com sucesso.");
-        menu();
-      } else if (cadastroResposta == 2) {
+      } else if (cadastroResposta === 2) {
         hospede.cadastrarHospede();
-        console.log("Hospede cadastrado com sucesso.");
-        menu();
+        console.log("Hóspede cadastrado com sucesso.");
+      } else if (cadastroResposta === 3) {
+        quarto.cadastrarQuarto();
+        console.log("Quarto cadastrado com sucesso!");
       }
       break;
 
     case 2:
-      if (opcaoAtualizar == 1) {
+      const opcaoAtualizar = atualizarOpcoes();
+      if (opcaoAtualizar === 1) {
         hospede.atualizarHospede();
-        menu();
-      } else if (opcaoAtualizar == 2) {
+      } else if (opcaoAtualizar === 2) {
         reserva.atualizarReserva();
-        menu();
-      } else if (opcaoAtualizar == 3) {
+      } else if (opcaoAtualizar === 3) {
         quarto.atualizarQuarto();
-        menu();
       }
       break;
 
     case 3:
-      if (opcaoDeletar == 1) {
+      const opcaoDeletar = deletarOpcoes();
+      if (opcaoDeletar === 1) {
         hospede.deletarHospede();
-        menu();
-      } else if (opcaoDeletar == 2) {
+      } else if (opcaoDeletar === 2) {
         reserva.deletarReserva();
-        menu();
-      } else if (opcaoDeletar == 3) {
+      } else if (opcaoDeletar === 3) {
         quarto.deletarQuarto();
-        menu();
       }
       break;
 
     case 4:
-      if (opcaoListar == 1) {
+      const opcaoListar = listarOpcoes();
+      if (opcaoListar === 1) {
         hospede.mostrarHospedes();
-        menu();
-      } else if (opcaoListar == 2) {
+      } else if (opcaoListar === 2) {
         quarto.mostrarQuartos();
-        menu();
-      } else if (opcaoListar == 3) {
+      } else if (opcaoListar === 3) {
         reserva.mostrarReservas();
-        menu();
       }
       break;
 
     default:
       console.log("Opção inválida. Tente novamente.");
   }
+
+  respostaMenu = menu(); // Chama o menu novamente para a próxima iteração
 }
 
+console.log("Saindo...");
 export { hospede };
--- src/Functions/functions.ts
+++ src/Functions/functions.ts
@@ -1,61 +1,48 @@
 const rl = require("readline-sync");
-import { Hospedes } from "../models/Hospedes";
 
 // menu
 const menu = (): number => {
   return parseInt(
     rl.question(
-      "=====MENU=====\n1 - Cadastrar reserva/hospede\n2 - Atualizar\n3 - Deletar\n4 - Listar\n5 - Sair"
+      "=====MENU=====\n1 - Cadastrar reserva/hospede\n2 - Atualizar\n3 - Deletar\n4 - Listar\n5 - Sair: "
     )
   );
 };
 
-let respostaMenu = menu();
-
 // cadastro
-const cadastroOpcoes = () => {
+const cadastroOpcoes = (): number => {
   return parseInt(
-    rl.question("1 - Cadastrar reserva\n2 - Cadastrar hospede:  ")
+    rl.question(
+      "1 - Cadastrar reserva\n2 - Cadastrar hospede\n3 - Cadastrar quarto: "
+    )
   );
 };
 
-let cadastroResposta: number = cadastroOpcoes();
-
 // atualizar
 const atualizarOpcoes = (): number => {
-  return parseInt(rl.question("1 - Atualizar hospede\n2 - Atualizar reserva"));
+  return parseInt(
+    rl.question(
+      "1 - Atualizar hospede\n2 - Atualizar reserva\n3 - Atualizar quarto: "
+    )
+  );
 };
-
-const opcaoAtualizar: number = atualizarOpcoes();
 
 // deletar
 const deletarOpcoes = (): number => {
   return parseInt(
-    rl.question("1 - Deletar hospede\n2 - Deletar reserva\n3 - Deletar quarto")
+    rl.question(
+      "1 - Deletar hospede\n2 - Deletar reserva\n3 - Deletar quarto: "
+    )
   );
 };
-
-const opcaoDeletar: number = deletarOpcoes();
 
 // listar
 const listarOpcoes = (): number => {
   return parseInt(
     rl.question(
-      "1 - Listar Hospedes\n2 - Listar Quartos\n3 - Listar Reservas\n4 - Voltar ao menu"
+      "1 - Listar Hospedes\n2 - Listar Quartos\n3 - Listar Reservas\n4 - Voltar ao menu: "
     )
   );
 };
 
-const opcaoListar: number = listarOpcoes();
-
-export {
-  cadastroResposta,
-  menu,
-  atualizarOpcoes,
-  opcaoAtualizar,
-  opcaoDeletar,
-  deletarOpcoes,
-  listarOpcoes,
-  opcaoListar,
-  respostaMenu,
-};
+export { cadastroOpcoes, menu, atualizarOpcoes, deletarOpcoes, listarOpcoes };
